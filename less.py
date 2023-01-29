9b95c28c347270abf09a96364bcfe0b9c936f73d
PiliposyanLusine <100078160+PiliposyanLusine@users.noreply.github.com>
2023-01-29 19:01:29 +0400

    create less.py

---
+++ less.py
@@ -0,0 +1,5 @@
+x=input()
+if x=='a' or x=='A' or x=='e' or x=='E' or x=='i' or x=='I' or x=='o' or x=='O' or x=='u' or x=='U' or x=='y' or x=='Y':
+    print('The character is a vowel')
+else:
+    print('The character is a consonant')
