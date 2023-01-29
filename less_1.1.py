086da0527bb76615ec020d13d0ce7ea6875c6487
PiliposyanLusine <100078160+PiliposyanLusine@users.noreply.github.com>
2023-01-29 17:44:16 +0400

    create less_1.1.py

---
+++ less_1.1.py
@@ -0,0 +1,27 @@
+x=input()
+if  (x)=='a':
+    print ('The character is a vowel')
+elif  (x)=='A':
+    print ('The character is a vowel')
+elif  (x)=='e':
+    print ('The character is a vowel')
+elif  (x)=='E':
+    print ('The character is a vowel')
+elif  (x)=='I':
+    print ('The character is a vowel')
+elif  (x)=='i':
+    print ('The character is a vowel')
+elif  (x)=='o':
+    print ('The character is a vowel')
+elif  (x)=='O':
+    print ('The character is a vowel')
+elif  (x)=='U':
+    print ('The character is a vowel')
+elif  (x)=='u':
+    print ('The character is a vowel')
+elif  (x)=='y':
+    print ('The character is a vowel')
+elif  (x)=='Y':
+    print ('The character is a vowel')
+else :
+    print ('The character is a consonant')
